d0ce0d829dfb72cf8d82b16240ecd56c8e67efab
Carlos O'Ryan <coryan@google.com>
2026-07-27 18:20:35 -0400

    chore: update librarian circa 2026-07-27 (#48)
    In this version, librarian generates correct cross-reference links for
    enums. This fixes a number of documentation warnings for
    `google-cloud-wkt` and many generated libraries.

--- Sources/GoogleCloudApigatewayV1/GetApiConfigRequest.swift
+++ Sources/GoogleCloudApigatewayV1/GetApiConfigRequest.swift
@@ -97,7 +97,7 @@ public struct GetApiConfigRequest: Codable, Equatable, GoogleCloudWkt._AnyPackab
 
     /// Initialize from a string value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownStringValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownStringValue`](doc:ConfigView/unknownStringValue(_:)).
     public init(stringValue: Swift.String) {
       switch stringValue {
       case "CONFIG_VIEW_UNSPECIFIED": self = .unspecified
@@ -109,7 +109,7 @@ public struct GetApiConfigRequest: Codable, Equatable, GoogleCloudWkt._AnyPackab
 
     /// Initialize from an integer value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownIntValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownIntValue`](doc:ConfigView/unknownIntValue(_:)).
     public init(intValue: Int) {
       switch intValue {
       case 0: self = .unspecified
