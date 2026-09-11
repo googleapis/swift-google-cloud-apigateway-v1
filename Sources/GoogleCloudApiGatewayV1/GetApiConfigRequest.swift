90664afe2dd58548ea819e7d967f4bb0e91d3e52
Carlos O'Ryan <coryan@google.com>
2026-09-11 20:11:51 +0000

    feat: update librarian with serialize-enums-as-strings (#829)
    Update librarian to use strings when serializing enums. Fixed a number
    of unit / integration tests.

--- Sources/GoogleCloudApiGatewayV1/GetApiConfigRequest.swift
+++ Sources/GoogleCloudApiGatewayV1/GetApiConfigRequest.swift
@@ -140,9 +140,9 @@ public struct GetApiConfigRequest: Codable, Equatable, GoogleCloudWKT._AnyPackab
     public func encode(to encoder: Encoder) throws {
       var container = encoder.singleValueContainer()
       switch self {
-      case .unspecified: return try container.encode(0)
-      case .basic: return try container.encode(1)
-      case .full: return try container.encode(2)
+      case .unspecified: return try container.encode("CONFIG_VIEW_UNSPECIFIED")
+      case .basic: return try container.encode("BASIC")
+      case .full: return try container.encode("FULL")
       case .unknownIntValue(let v): return try container.encode(v)
       case .unknownStringValue(let v): return try container.encode(v)
       }
